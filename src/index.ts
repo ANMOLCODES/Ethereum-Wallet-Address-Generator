59e101eed5be1d9b74c8cdfa7022f6faa8c55258
anmolarora.eth <mr.anmolarora@outlook.com>
2023-03-20 09:58:44 +0530

    Final Commit

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
 import { ethers } from "ethers";
+import { writeFileSync } from "fs";
 
-const wallet = ethers.Wallet.createRandom();
-console.log(wallet);
+// write a for loop to create 30 wallets
+for (let i = 0; i < 30; i++) {
+  const wallet = ethers.Wallet.createRandom();
+  console.log(wallet);
+  const data = {
+    address: wallet.address,
+    mnemonic: wallet.mnemonic.phrase,
+  };
+  // write the data to a file and append
+  writeFileSync("wallets.json", JSON.stringify(data, null, 2), {
+    flag: "a",
+  });
+  writeFileSync("addresses.txt", '"' + data.address + '"' + " ,", {
+    flag: "a",
+  });
+}
